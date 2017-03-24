e27626277e89a12d808b6594659e6a763d2cad3b
Elise Chant <elise.chant@gmail.com>
2017-03-24 12:48:41 +1100

    feat(chart): adds chart wrapper for react (#6)
    * feat(chart): adds chart wrapper for react
    
    * feat(chart): promote Highcharts initialisation to a HOC
    
    * feat(chart): change withChart to withHighcharts; adds bar as well as pie;
    
    * feat(chart): move chartConfig to component;

---
+++ src/hocs/withHighcharts.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import Highcharts from 'highcharts';
+
+import {onNextFrame} from './../utils/DOM';
+
+
+export const withChart = ComposedComponent => props => {
+
+  let chart;
+
+  const createFn = (el, chartConfig, chartOptions) => {
+    chart = new Highcharts.Chart({
+      chart: {
+        renderTo: el,
+        ...chartConfig
+      },
+      ...chartOptions
+    });
+  };
+
+  const updateFn = (chartConfig, chartOptions) => {
+    createFn(chartConfig, chartOptions);
+    onNextFrame(chart.reflow);
+  };
+
+  const destroyFn = () => {
+    chart.destroy();
+  };
+
+  return <ComposedComponent create={createFn}
+                            update={updateFn}
+                            destroy={destroyFn} {...props} />
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import React from 'react';
+import {render} from 'react-dom';
+
+import BarChart from './components/barChart';
+import ColumnChart from './components/columnChart';
+
+/*
+
+  library
+
+  : export pie
+  : export line
+  : export bar
+  : export ...
+
+ */
+
+export default {
+  BarChart,
+  ColumnChart
+};
+
+
+const chartOptions = {
+  title: {
+    text: 'Fruit Consumption'
+  },
+  xAxis: {
+    categories: ['Apples', 'Bananas', 'Oranges']
+  },
+  yAxis: {
+    title: {
+      text: 'Fruit eaten'
+    }
+  },
+  series: [{
+    name: 'Jane',
+    data: [1, 0, 4]
+  }, {
+    name: 'John',
+    data: [5, 7, 3]
+  }]
+};
+
+render(
+  <ColumnChart chartOptions={chartOptions} />, document.getElementById('root')
 );
+
