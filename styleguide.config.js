a563a1bfebc0ea2ae588876ef383dde6450638f6
Elise Chant <elise.chant@gmail.com>
2017-04-02 11:23:35 +1000

    chore(docs): bump styleguidist version (#15)
    * chore(styleguide): update style guide and include new webpack config for it
    
    * chore(docs): remove redundant file

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,28 +2,14 @@ var path = require('path');
 
 module.exports = {
   title: '@gov.au/datavizkit',
-  components: './src/components/**/*.js',
+  sections: [
+    {
+      name: 'Widgets',
+      components: './src/components/widgets/**/*.js',
+    },
+  ],
   styleguideDir: 'docs',
   defaultExample: true,
   serverPort: 4000,
-  updateWebpackConfig(webpackConfig) {
-    // Your source files folder or array of folders, should not include node_modules
-    const dir = path.join(__dirname, 'src');
-    webpackConfig.module.loaders.push(
-      // Babel loader will use your project’s .babelrc
-      {
-        test: /\.js?$/,
-        include: dir,
-        loader: 'babel'
-      },
-      // Other loaders that are needed for your components
-      {
-        test: /\.css$/,
-        include: dir,
-        loader: 'style!css?modules&importLoaders=1'
-      }
-    );
-    webpackConfig.devtool = 'eval';
-    return webpackConfig;
-  },
+  webpackConfig: require('./config/webpack.config.styleguide.js')
 };
