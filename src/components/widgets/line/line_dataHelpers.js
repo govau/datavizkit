c6269aa95fb46a0813f2f42f20bbd3f8a79cb3ea
elisechant <elise.chant@gmail.com>
2017-04-02 13:25:58 +1000

    feat(iine): enable shared crosshairs and tooltip

--- src/components/widgets/line/line_dataHelpers.js
+++ src/components/widgets/line/line_dataHelpers.js
@@ -34,11 +34,12 @@ export const makeChartOptions = ({
         },
         allowPointSelect: false,
       },
-      // tooltip: {
-      //   enabled: true,
-      //   shared: true,
-      //   crosshairs: true,
-      // }
+    },
+
+    tooltip: {
+      enabled: true,
+      shared: true,
+      crosshairs: true,
     },
 
     // instance props
@@ -46,10 +47,11 @@ export const makeChartOptions = ({
       name: 'Installation',
       data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
     },
-      // {
-      // name: 'Manufacturing',
-      // data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    // }, {
+      {
+      name: 'Manufacturing',
+      data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    },
+    // {
     //   name: 'Sales & Distribution',
     //   data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
     // }, {
