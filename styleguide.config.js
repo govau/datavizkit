61f87299ce434e74d8f43bb3f6625bc547ce3a40
elisechant <elise.chant@gmail.com>
2017-04-03 16:39:02 +1000

    docs(styleguide): improve

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,13 +2,14 @@ var path = require('path');
 
 module.exports = {
   title: '@gov.au/datavizkit',
+  template: './docs/template.html',
   sections: [
     {
       name: 'Widgets',
       components: './src/components/widgets/**/*.js',
     },
   ],
-  styleguideDir: 'docs',
+  styleguideDir: 'docs/.out',
   defaultExample: true,
   serverPort: 4000,
   webpackConfig: require('./config/webpack.config.styleguide.js')
