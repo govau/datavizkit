b4f149e27c4832ef1a89ffd221df8f40a6ea8471
Josh Mostafa <micapam@gmail.com>
2017-04-10 09:29:00 +1000

    WIP sparkline

---
+++ src/components/widgets/sparkline/sparkline_dataHelpers.js
@@ -0,0 +1,113 @@
+
+// import Highcharts from 'highcharts';
+import last from 'lodash/last';
+
+
+export const makeChartOptions = ({
+  emitSetState = () => {},
+  widget,
+}) => {
+  return {
+    // default options
+    chart: {
+      type: 'line',
+      events: {
+        load: function() {
+          let customLegendData = this.series.map(s => {
+            const lastData = last(s.data);
+            return {
+              key: lastData.category,
+              y: lastData.y,
+              seriesName: s.name,
+              color: lastData.color
+            }
+          });
+          emitSetState({'customLegend': customLegendData});
+        }
+      },
+    },
+    title: {
+      text: widget.title,
+      align: 'left',
+    },
+    subtitle: {
+      useHTML: true,
+      text: `<span>Last updated <time dateTime="${widget.dateUpdated}">${widget.dateUpdated}</time></span>`,
+      align: 'left',
+    },
+    xAxis: {
+      crosshair: true,
+      // type: 'datetime', // todo - format x labels to datetime
+      // Format 24 hour time to AM/PM
+      // dateTimeLabelFormats: {
+      //   hour: '%I:%M %P',
+      //   minute: '%I %M'
+      // },
+      // labels: {
+      //   formatter: function() {
+      //     return Highcharts.dateFormat('%I:%M %P', this.value);
+      //   }
+      // }
+    },
+    plotOptions: {
+      line: {
+        animation: false,
+        allowPointSelect: true,
+        stickyTracking: true
+      },
+      series: {
+        animation: false,
+        point: {
+          events: {
+            mouseOver: function() {
+              const sliceIdx = this.index;
+              const customLegendData = this.series.chart.series.map(s => {
+                const sliceData = s.data[sliceIdx];
+                return {
+                  key: sliceData.category,
+                  y: sliceData.y,
+                  seriesName: s.name,
+                  color: sliceData.color
+                }
+              });
+              emitSetState({'customLegend': customLegendData});
+            },
+          }
+        },
+        states: {
+          select: { // required because can be selected programatically
+            enabled: false
+          }
+        },
+        allowPointSelect: false,
+      },
+    },
+
+    tooltip: {
+      enabled: true,
+      shared: true,
+      crosshairs: true,
+    },
+
+    // instance props
+    series: [{
+      name: 'Installation',
+      data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    },
+      {
+      name: 'Manufacturing',
+      data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    },
+    // {
+    //   name: 'Sales & Distribution',
+    //   data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    // }, {
+    //   name: 'Project Development',
+    //   data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+    // }, {
+    //   name: 'Other',
+    //   data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+    // }
+    ]
+  };
+};
---
+++ src/components/widgets/sparkline/sparkline_widget.js
@@ -0,0 +1,46 @@
+import React, {PureComponent} from 'react';
+import Chart from './../../chart';
+import {makeChartOptions} from './sparkline_dataHelpers';
+import Legend from './../../customLegend';
+
+/**
+ * Renders a Line Widget with its surrounding state.
+ */
+class LineWidget extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.proxiedSetState = this.proxiedSetState.bind(this);
+    this.chartInstance = null;
+    this.state = {
+      customLegend: []
+    };
+  }
+
+  proxiedSetState(state) {
+    this.setState(state);
+  }
+
+  render() {
+    const {customLegend} = this.state;
+
+    const chartOptions = makeChartOptions({
+      emitSetState: this.proxiedSetState,
+      widget: this.props.widget,
+    });
+
+    return (
+      <article className={`chart--line`} role="article">
+        <section>
+          <Chart ref={el => this.chartInstance = el} options={chartOptions} />
+          <div>
+            {customLegend && customLegend.length && <Legend data={customLegend} />}
+          </div>
+        </section>
+      </article>
+    )
+  }
+
+}
+
+export default LineWidget;
+
