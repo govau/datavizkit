953e2e787caf61fdd45039b3c69eea76d82c4844
elisechant <elise.chant@gmail.com>
2017-04-14 10:24:00 +1000

    able to export to lib

---
+++ src/Datavizkit.js
@@ -0,0 +1,21 @@
+
+
+/*
+
+Exports the library
+
+ */
+
+import ColumnWidget from './components/widgets/column';
+import LineWidget from './components/widgets/line';
+import SparklineWidget from './components/widgets/sparkline';
+import DonutWidget from './components/widgets/donut';
+import StackedColumnWidget from './components/widgets/stackedColumn';
+
+export {
+  ColumnWidget,
+  LineWidget,
+  SparklineWidget,
+  DonutWidget,
+  StackedColumnWidget
+}
