2b39bf006e76be4d8f591de4ceb0b2b844fd7190
elisechant <elise.chant@gmail.com>
2017-04-27 14:40:58 +1000

    remove not required header

--- src/components/heroWidget/heroWidget.js
+++ src/components/heroWidget/heroWidget.js
@@ -1,17 +1,15 @@
+
 import React from 'react';
 import {compose} from 'recompose';
 
 import withHighcharts from './../withHighcharts';
 import withHeroChart from './../withHeroChart';
 
-// render a uniquely marked up and styled custom ColumnWidget
-// might also have a ColumnWidgetLarge or ColumnWidgetMonochrome
+
+// render a uniquely marked up and styled custom HeroWidget
 const HeroWidget = (props) => {
   return (
     <article className="chart--hero" role="article">
-      <header>
-        <div>Hero Widget</div>
-      </header>
       <section>{props.children}</section>
     </article>
   )
