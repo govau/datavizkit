8a4a1a1ada93d7eaa7df19a77be5c01791f001b0
Jon Alport <jon.alport@digital.gov.au>
2018-02-01 17:08:34 +1100

    Add solid gauge, bump highcharts version
    No breaking changes migrating from v5->6 according to:
    https://www.highcharts.com/blog/news/announcing-highcharts-6/

--- src/components/abstractWidget/abstractWidget.js
+++ src/components/abstractWidget/abstractWidget.js
@@ -1,11 +1,8 @@
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
-
 import AbstractChart from './abstractChart';
-import {validAxisType, validChartType} from './../../helpers/propsValidators';
-// import {validSeriesData} from './../../helpers/customValidators'; // todo 
+import { validChartType } from './../../helpers/propsValidators';
 
 class AbstractWidget extends Component {
   getChart() {
@@ -18,7 +15,7 @@ class AbstractWidget extends Component {
     if (get(config, 'chart.renderTo')) {
       delete config.chart.renderTo;
     }
-  
+
     return (
       <div>
         <AbstractChart ref={(c) => { this.abstractChart = c; }} config={config} />
@@ -29,57 +26,29 @@ class AbstractWidget extends Component {
 
 if (__DEV__) {
   AbstractWidget.propTypes = {
-
     config: PropTypes.shape({
-
       chart: PropTypes.shape({
         type: validChartType,
-        height: PropTypes.number,
-        width: PropTypes.number,
+        height: PropTypes.oneOfType([
+          PropTypes.number,
+          PropTypes.string,
+        ]),
+        width: PropTypes.oneOfType([
+          PropTypes.number,
+          PropTypes.string,
+        ]),
       }),
-
-      tooltip: PropTypes.shape({
-
-      }),
-
+      tooltip: PropTypes.shape({}),
       yAxis: PropTypes.oneOfType([
         PropTypes.object,
         PropTypes.array,
       ]),
-
       xAxis: PropTypes.oneOfType([
         PropTypes.object,
         PropTypes.array,
       ]),
-
-      // yAxis: PropTypes.arrayOf(PropTypes.shape({ // todo - validateAxisData
-      //   categories: PropTypes.array,
-      //   type: validAxisType,
-      // })),
-      // xAxis: PropTypes.arrayOf(PropTypes.shape({
-      //   categories: PropTypes.array,
-      //   type: validAxisType,
-      // })),
-
       series: PropTypes.array.isRequired, // todo - validSeriesData
-
-      // series: PropTypes.arrayOf(PropTypes.shape({
-      //   data: PropTypes.arrayOf(
-      //     PropTypes.oneOf([
-      //       PropTypes.shape({
-      //         x: PropTypes.number.isRequired,
-      //         y: PropTypes.number.isRequired,
-      //         z: PropTypes.number,
-      //         name: PropTypes.string.isRequired,
-      //         units: PropTypes.string,
-      //         color: PropTypes.string,
-      //       }),
-      //       PropTypes.array,
-      //     ])
-      //   ).isRequired,
-      // }).isRequired,),
     })
-
   };
 }
 
--- src/helpers/propsValidators.js
+++ src/helpers/propsValidators.js
@@ -1,16 +1,33 @@
-
 import PropTypes from 'prop-types';
 
-
 export const validAxisType = PropTypes.oneOf([
-  'category', 'linear', 'logarithmic', 'datetime'
+  'category',
+  'linear',
+  'logarithmic',
+  'datetime'
 ]);
 
 export const validChartType = PropTypes.oneOf([
-  'area', 'arearange', 'areaspline', 'areasplinerange',
-  'bar', 'boxplot', 'bubble', 'candlestick', 'column',
-  'columnrange', 'errorbar', 'flags', 'funnel',
-  'line', 'ohlc', 'pie', 'polygon', 'pyramid', 'scatter',
-  'spline', 'waterfall'
+  'area',
+  'arearange',
+  'areaspline',
+  'areasplinerange',
+  'bar',
+  'boxplot',
+  'bubble',
+  'candlestick',
+  'column',
+  'columnrange',
+  'errorbar',
+  'flags',
+  'funnel',
+  'line',
+  'ohlc',
+  'pie',
+  'polygon',
+  'pyramid',
+  'scatter',
+  'solidgauge',
+  'spline',
+  'waterfall'
 ]);
-
